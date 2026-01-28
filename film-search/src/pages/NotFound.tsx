e6b4b78f01884aaa6be3ec5f5fe9b50cbdb1fb0c
nutnut7 <dragoneve2006@gmail.com>
2026-01-28 11:54:20 +0100

    ADD: aggiunto effetto custom per pagina 404

--- film-search/src/pages/NotFound.tsx
+++ film-search/src/pages/NotFound.tsx
@@ -1,14 +1,31 @@
 //Pagina 404 - Not Found
 import {Link} from "react-router-dom";
+import FuzzyText  from "../components/FuzzyText";
 
 export const NotFound =  () => {
     return(
         <div className="not-found">
             <div className="not-found-content">
-                <h1 className="not-found-title">404</h1>
-                <h2>Pagina non trovata</h2>
-                <p>La pagina che cerchi non esiste 😭</p>
-                <Link to="/" className="btn btn-primary">
+                <FuzzyText
+                    fontSize={60}
+                    fontWeight={900}
+                    baseIntensity={0.3}
+                    hoverIntensity={0.8}
+                    glitchMode={true}
+                    glitchInterval={3000}
+                    glitchDuration={300}
+                    enableHover={true}
+                    clickEffect={true}
+                    direction="both"
+                    gradient={['#3b82f6', '#2563eb', '#1e40af']}
+                    letterSpacing={10}
+                    className="mb-8"
+                >
+                    404
+                </FuzzyText>
+                    <h2>Pagina non trovata</h2>
+                    <p>La pagina che cerchi non esiste 😭</p>
+                        <Link to="/" className="btn btn-primary">
                     Torna alla HOME
                 </Link>
             </div>
